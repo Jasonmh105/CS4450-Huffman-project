baec8b28303cdbcb4bab565d427a3c85c097d485
Jason Hodges <jasonmh105@outlook.com>
2026-04-26 06:00:07 +0000

    new handling for tree generation and node toString

--- Algorithm/binaryTree.py
+++ Algorithm/binaryTree.py
@@ -16,12 +16,33 @@ class Node():
     
     def getWeight(self):
         return self.weight
+    
+    # Displayes raw data of called node and all those under it in DFS
+    def __str__(self):
+        return f'node({self.left}, {self.right}, {self.word}, {self.weight})'
 
 class BinaryTree():
     def __init__(self):
         self.root = None
 
+    # Handle new node in huffman tree
     def insert(self, tuple):
         newNode = Node()
         newNode.setWord(tuple[0])
         newNode.setWeight(tuple[1])
+        #continue with insertion logic
+
+    # Generate tree root and nodes
+    def startTree(self, tuple1, tuple2):
+        self.root = Node()
+        self.root.left = Node()
+        self.root.right = Node()
+        self.root.weight = tuple1[1]+tuple2[1]
+
+        self.root.left.setWord(tuple1[0])
+        self.root.left.setWeight(tuple1[1])
+
+        self.root.right.setWord(tuple2[0])
+        self.root.right.setWeight(tuple2[1])
+
+        print(self.root)
--- Algorithm/huffman.py
+++ Algorithm/huffman.py
@@ -5,7 +5,7 @@ def huffmanAlgorithm(tuples):
     # Sort tuples by frequency
     tuplesSorted = sorted(tuples, key=lambda element: element[1])
 
-    testTree = BinaryTree()
-    testTree.insert(tuplesSorted[0])
+    huffmanTree = BinaryTree()
+    huffmanTree.startTree(tuplesSorted[0],tuplesSorted[1])
 
     return tuplesSorted
