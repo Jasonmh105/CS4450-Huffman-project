16ae2bdd74a582263fe66c4cb3dbc36a69202244
Jason Hodges <jasonmh105@outlook.com>
2026-04-28 04:16:22 +0000

    removed binary_tree.py and BinaryTree() class within, as no objects were necessary from this class. Moved binary tree management logic to huffman.py along with Node() class

--- Algorithm/huffman.py
+++ Algorithm/huffman.py
@@ -1,4 +1,50 @@
-from Algorithm.binary_tree import BinaryTree
+class Node():
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.word = ""
+        self.weight = 0
+
+    def set_word(self, word):
+        self.word = word
+
+    def set_weight(self, weight):
+        self.weight = weight
+
+    def get_word(self):
+        return self.word
+    
+    def get_weight(self):
+        return self.weight
+    
+    # Displayes raw data of called node and all those under it in DFS
+    def __str__(self):
+        return f'node({self.left}, {self.right}, {self.word}, {self.weight})'
+    
+    # operator overloading so that nodes can be compared
+    def __lt__(self, other):
+        return self.get_weight() < other.get_weight()
+    def __gt__(self, other):
+        return self.get_weight() > other.get_weight()
+
+def tuples_to_nodes(tuples):
+
+        nodes = []
+        for each in tuples:
+            new_node = Node()
+            new_node.set_word(each[0])
+            new_node.set_weight(each[1])
+            nodes.append(new_node)
+
+        return nodes
+
+def merge(node1, node2):
+        new_root = Node()
+        new_root.left = node1
+        new_root.right = node2
+        new_root.set_weight(node1.get_weight() + node2.get_weight())
+
+        return new_root
 
 def huffman_algorithm(tuples):
     
@@ -6,16 +52,15 @@ def huffman_algorithm(tuples):
     tuples_sorted = sorted(tuples, key=lambda element: element[1])
     print(tuples_sorted, "\n")
 
-    # Start tree and convert each tuple to a tree node
-    huffman_tree = BinaryTree()
-    node_list = huffman_tree.tuples_to_nodes(tuples_sorted)
+    # Convert each tuple to a tree node
+    node_list = tuples_to_nodes(tuples_sorted)
 
     # Recursivly merge nodes until only one is left
     while(len(node_list) > 1):
         # Make new root node from first 2 in node_list, then remove merged nodes, reinsert new root, and sort
         # print(huffman_tree.merge(node_list[0],node_list[1]), "\n")
         # Append to node_list, the root of merged first 2 nodes in list
-        node_list.append(huffman_tree.merge(node_list[0],node_list[1]))
+        node_list.append(merge(node_list[0],node_list[1]))
         # Remove the 2 nodes that have been merged together
         node_list.pop(0)
         node_list.pop(0)
