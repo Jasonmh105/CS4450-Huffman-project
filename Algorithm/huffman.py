77717b223ff7d266c7bd1b5c5f42d6556d54705a
Jason Hodges <jasonmh105@outlook.com>
2026-04-28 04:01:01 +0000

    commented out redundent print statement, and used pop() function on node_list for clarity

--- Algorithm/huffman.py
+++ Algorithm/huffman.py
@@ -13,12 +13,12 @@ def huffman_algorithm(tuples):
     # Recursivly merge nodes until only one is left
     while(len(node_list) > 1):
         # Make new root node from first 2 in node_list, then remove merged nodes, reinsert new root, and sort
-        print(huffman_tree.merge(node_list[0],node_list[1]), "\n")
+        # print(huffman_tree.merge(node_list[0],node_list[1]), "\n")
         # Append to node_list, the root of merged first 2 nodes in list
         node_list.append(huffman_tree.merge(node_list[0],node_list[1]))
         # Remove the 2 nodes that have been merged together
-        node_list.remove(node_list[0])
-        node_list.remove(node_list[0])
+        node_list.pop(0)
+        node_list.pop(0)
         # Re-sort node_list so that the new tree is in the proper place
         node_list.sort()
 
