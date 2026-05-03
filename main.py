171b4a1efd8bb69e869dcb8152e009d93a11bc1e
Stefin Racho <106283464+stefinracho@users.noreply.github.com>
2026-05-03 08:39:20 -0700

    feat: print welcome message (#6)

--- main.py
+++ main.py
@@ -12,15 +12,34 @@ from user_input import gather_data
 
 #     return alpha_freq_list
 
+
+def display_welcome_msg() -> None:
+    print("""
+==========================================================================
+                       Welcome to Huffman Encoder!
+==========================================================================
+
+This program turns your alphabet into codewords using Huffman encoding.
+
+To create your alphabet you'll be asked to input at least 2 strings (e.g.,
+hello world). Each string is separated by a space. Then, for each string
+you will be asked to input how many times that string occurs (e.g, 42).
+Finally, you will see what your alphabet looks like when it is encoded!
+""")
+
+
 def main():
-    
+    display_welcome_msg()
+
     elements = gather_data()
-    
+
     # Compute huffman algorithm
     encoded_alphabet = huffman_algorithm(elements)
 
     # Display results
     print("PRINTING FINAL ENCODED ALPHABET:     ", encoded_alphabet)
 
+
 if __name__ == "__main__":
     main()
+
